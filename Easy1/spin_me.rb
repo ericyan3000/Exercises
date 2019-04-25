e6c05762b3947f965f5d337613ac25c81eea78fc
Eric Yan <ericyan3000@gmail.com>
2019-04-25 07:39:51 -0700

    4/25

---
+++ Easy1/spin_me.rb
@@ -0,0 +1,10 @@
+def spin_me(str)
+  str.split.each do |word|
+    word.reverse!
+  end
+end
+
+str = 'hello world'
+puts str.object_id # 47435609148580
+puts spin_me(str).object_id # 47435609148360
+puts str
