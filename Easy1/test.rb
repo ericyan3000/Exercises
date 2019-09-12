6af2a152165a7bf9b57706fd38c5bee950d64ee5
ericyan3000 <ericyan3000@gmail.com>
2019-09-11 23:19:21 -0700

    Delete test.rb

--- Easy1/test.rb
+++
@@ -1,8 +0,0 @@
-def title_case(title, minor_words = '')
-  title.capitalize.split().map {|a| 
-  	p a
-  	minor_words.downcase.split().include?(a) ? a : a.capitalize
-  }.join(' ')
-end
-
-p title_case('A LION king of MY heart', 'my')
